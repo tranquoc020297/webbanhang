6581d4fa1c44c8d93c56416b8791c01549bfe02b
Quoc Dep Trai <tranquoc020297@gmail.com>
2017-11-20 23:25:23 +0700

    day la lan dau tui post len

---
+++ page/Highlights.php
@@ -0,0 +1,87 @@
+<div class="marquee" style="text-align:center">
+<p style="font-size:20pt">Sản Phẩm Nổi Bật</p>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+        <a href=""><img src="source/images/lamborghini/6.jpg" width="200px"height="140px" >Lamborghini AVENTADOR S 2017</a>
+        </div>
+        <div class="col-sm-3">
+            <a href=""><img src="source/images/lexus/3.jpg"width="200px" height="140px">lexus ES350</a>
+        </div>
+        <div class="col-sm-3">
+        <a href=""><img src="source/images/ferrari/1.jpg"width="200px"height="140px" >Ferrari LAFERRARI</a>
+        </div>
+        <div class="col-sm-3">
+        <a href=""><img src="source/images/audi/10.jpg" width="200px"height="140px">Audi A8</a>
+        </div>
+    </div>
+    
+    <div class="news" style="text-align:center">
+    <h3>Tin Tức Sản Phẩm</h3>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="#"><img src="source/images/ferrari/1.jpg"width="200px" alt="">   </a>
+                </div>
+                <div class="col-md-6">
+                    <a href="">LaFerrari - siêu xe mạnh nhất của Ferrari</a>
+                    Siêu xe sử dụng tổng hợp động cơ xăng và điện, công suất 950 mã lực và chỉ sản xuất giới hạn 499 chiếc... <a href="">xem thêm</a>
+                    
+                    
+
+                </div>
+        </div>
+            </div>
+        
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                    <a href=""><img src="source/images/lamborghini/1.jpg" width="200px" alt=""></a>
+                </div>
+                <div class="col-md-6">
+                <a href="">Lamborghini Aventador SV Độc Nhất VN</a>
+
+                Mức giá này tính ra có lẽ cũng không quá cao so với 1 siêu xe phiên bản giới hạn đời mới, mang số hiệu 
+                1/500... <a href="">xem thêm</a> 
+                </div>
+            </div>
+        </div>
+        <div style="height:10%">&nbsp;</div>
+     
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="#"><img src="source/images/audi/10.jpg"width="200px"height="151px" alt="">   </a>
+                </div>
+                <div class="col-md-6">
+                    <a href="">Chiêm ngưỡng Audi A8 2018 “bằng xương, bằng thịt</a>
+                    Audi A8 2018 vừa chính thức được giới thiệu ra thị trường toàn cầu tại sự kiện Audi Summit với... <a href="">xem thêm</a>
+                    
+
+                </div>
+        </div>
+            </div>
+        
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                    <a href=""><img src="source/images/lexus/1.jpg" width="200px" height="150" alt=""></a>
+                </div>
+                <div class="col-md-6">
+                <a href="">Lexus ES 250 2017-2018: thông số kỹ thuật, giá bán</a>
+
+                là mẫu sedan bán chạy nhất của Lexus tại Việt nam. Sau đây là hình ảnh chi tiết, thông số kỹ thuật.... <a href="">xem thêm</a> 
+                </div>
+            </div>
+        </div>
+        <div style="height:10%">&nbsp;</div>
+     
+    </div>
+</div>
---
+++ page/detail.php
@@ -0,0 +1,34 @@
+<hr>
+<div class="container">
+<p><b>LaFerrari - siêu xe mạnh nhất của Ferrari 87 Tỷ</b>&nbsp;&nbsp;<button style="right:3%; float:right" class="btn btn-outline-info"><span><i class="fa fa-shopping-cart" aria-hidden="true"></i>Đặt Mua</span></button> </p>
+Siêu xe sử dụng tổng hợp động cơ xăng và điện, công suất 950 mã lực và chỉ sản xuất giới hạn 499 chiếc.
+<br>
+<img src="source/images/ferrari/1.jpg"height="350"width="575"  alt=""><br>
+Ferrari LaFerrari là mẫu siêu xe ra đời thay thế huyền thoại Ferrari Enzo.<br>
+<img src="source/images/ferrari/LaFerrari/1.jpg"height="350"width="auto" alt=""><br>
+Siêu xe mang thiết kế tổng hợp của F12 Berlinetta và Enzo, công nghệ F1, sản xuất giới hạn 499 chiếc.<br>
+<img src="source/images/ferrari/LaFerrari/2.jpg"height="350"width="auto" alt=""><br>
+Vì những kỹ sư của Ferrari cho rằng sức mạnh 789 mã lực của động cơ 6,3 lít V12 đặt giữa là chưa đủ, nên thêm vào một động cơ điện cho công suất 161 mã lực và mô-men xoắn cực đại 270 Nm.
+<br>
+<img src="source/images/ferrari/LaFerrari/3.jpg"height="350"width="auto" alt=""><br>
+Động cơ điện được đặt sau hộp số ly hợp kép 7 cấp, khối pin nặng 60 kg. Tổng sức mạnh của xe là 950 mã lực, mô-men xoắn cực đại 899 Nm.
+<br><img src="source/images/ferrari/LaFerrari/4.jpg"height="350"width="auto" alt="">
+<br>Siêu xe tăng tốc từ 0-96 km/h chưa đến 3 giây, tốc độ tối đa khoảng 446 km/h.
+<br><img src="source/images/ferrari/LaFerrari/5.jpg"height="350"width="auto" alt="">
+<br>Giống như các mẫu xe F1 của Ferrari, khung và thân xe LaFerrari được sản xuất thủ công từ sợi carbon.
+
+<br><img src="source/images/ferrari/LaFerrari/6.jpg"height="350"width="auto" alt="">
+<br>Luồng không khí di chuyển xung quanh siêu xe luôn được thay đổi thích ứng với trạng thái di chuyển.
+<br><img src="source/images/ferrari/LaFerrari/7.jpg"height="350"width="auto" alt="">
+<br>Thiết kế khí động học chủ động cũng đóng vai trò quan trọng, gồm cánh hướng gió trước linh hoạt, cánh dưới sàn xe, cánh gió sau cùng bộ khuếch tán sau chủ động.
+<br><img src="source/images/ferrari/LaFerrari/8.jpg"height="350"width="auto" alt="">
+<br>Tại tốc độ 201 km/h, lực ép xuống mặt đường vào khoảng 500 kg.
+<br><img src="source/images/ferrari/LaFerrari/9.jpg"height="350"width="auto" alt="">
+<br>Lốp trước Perelli P Zero lớn hơn so với Enzo, và cụm phanh carbon ceramic thế hệ mới Brembo.
+<br><img src="source/images/ferrari/LaFerrari/10.jpg"height="350"width="auto" alt="">
+<br>LaFerrari trang bị những tính năng an toàn như hệ thống cân bằng điện tử ESC, kiểm soát hành trình điện tử F1 EF1-TRAC, hệ thống chống bó cứng phanh ABS và phân bổ lực phanh điện tử EBD, bộ vi sai thế hệ thứ 3.
+<img src="source/images/ferrari/LaFerrari/11.jpg"height="350"width="auto" alt="">
+<div>&nbsp;&nbsp;</div>
+<img src="source/images/ferrari/LaFerrari/12.jpg"height="350"width="auto" alt="">
+<div>&nbsp;&nbsp;</div>
+</div>
